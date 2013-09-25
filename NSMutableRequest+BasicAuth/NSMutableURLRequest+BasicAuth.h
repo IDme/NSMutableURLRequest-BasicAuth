1a0141c9be26d875fede1aed878eab65d8414c8a
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2013-09-25 17:44:39 -0400

    Added missing header

--- NSMutableRequest+BasicAuth/NSMutableURLRequest+BasicAuth.h
+++ NSMutableRequest+BasicAuth/NSMutableURLRequest+BasicAuth.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 ID.me, Inc. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 @interface NSMutableURLRequest (BasicAuth)
 
 + (void)basicAuthForRequest:(NSMutableURLRequest *)request withUsername:(NSString *)username andPassword:(NSString *)password;
