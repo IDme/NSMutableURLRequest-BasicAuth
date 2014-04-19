4a8e09d636965fef2f326ad808ffe2f5ea3733cf
Mike Abdullah <me@mikeabdullah.net>
2014-04-19 11:28:57 +0100

    Bring in a simple test
    Make sure encoded auth info conforms to the spec

---
+++ Tests/NSMutableRequest_BasicAuth_Tests.m
@@ -0,0 +1,27 @@
+//
+//  NSMutableRequest_BasicAuth_Tests.m
+//  NSMutableRequest+BasicAuth_Tests
+//
+//  Created by Mike on 19/04/2014.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "NSMutableURLRequest+BasicAuth.h"
+
+
+@interface NSMutableRequest_BasicAuth_Tests : XCTestCase
+
+@end
+
+
+@implementation NSMutableRequest_BasicAuth_Tests
+
+- (void)testSimpleCase
+{
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://example.com"]];
+    [NSMutableURLRequest basicAuthForRequest:request withUsername:@"Aladdin" andPassword:@"open sesame"];
+	XCTAssertEqualObjects([request valueForHTTPHeaderField:@"Authorization"], @"Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
+}
+
+@end
